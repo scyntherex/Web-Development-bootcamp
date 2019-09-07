a0b109295007f51ac4f50feff13a0f98debb0c2c
unknown <rone.caballero@gmail.com>
2019-09-06 19:00:31 -0700

    Added Exercise solutions with js script.

---
+++ JavaScript/script.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your Last name?");
+var userAge = prompt("What is age?");
+
+console.log("Very great to meet ya, " + userFirstName + " " + userLastName);
+console.log("Minutess alive: " + (userAge * 525600));
