42e33abc2d2f07e03eee9fbb54fec6d9ea33f6d9
unknown <rone.caballero@gmail.com>
2019-09-12 20:33:21 -0700

    updated to do list app.

--- JavaScript/list.js
+++ JavaScript/list.js
@@ -1,16 +1,34 @@
-var todos = ["Feed cats"];
+var todos = ["Feed cats", "Clean Litter Box"];
 
 var input = prompt("What to do?");
 
 while (input !== "quit") {
 	if(input === "list") {
-		console.log(todos);
+		listTodos();
 	} else if(input === "new") {
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
+		addTodo();
+	} else if(input === "delete") {
+		removeTodo();
 	}
-
 	input = prompt("What to do?");
 
 }
 console.log("Quitted app.");
+
+function listTodos() {
+	todos.forEach(function(todo, i) {
+			console.log(i + ": " + todo);
+	})
+}
+
+function addTodo() {
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+	console.log("Added item to list.");
+}
+
+function removeTodo() {
+	var index = prompt("Enter index of th elist to delete.");
+	todos.splice(index, 1);
+	console.log("Removed item from list.");
+}
