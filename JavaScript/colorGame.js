6922f3a9503b57d26a3fc379358e5fc0afe6d7c0
unknown <rone.caballero@gmail.com>
2019-09-19 17:46:36 -0700

    Added random color generator.

--- JavaScript/colorGame.js
+++ JavaScript/colorGame.js
@@ -1,4 +1,4 @@
-
+/*
 var colors = [
 	"rgb(255, 0, 0)",
 	"rgb(255, 255, 0)",
@@ -6,11 +6,14 @@ var colors = [
 	"rgb(0, 255, 255)",
 	"rgb(0, 0, 255)",
 	"rgb(255, 0, 255)",
-]
+]*/
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisp");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,9 +27,48 @@ for(var i = 0; i < squares.length; i++) {
 		var clickedColor = this.style.backgroundColor;
 		//compare color to picked color.
 		if(clickedColor === pickedColor) {
-			alert("CORRECT!");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		} else {
-			alert("WRONG!");
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again."
 		}
 	});
+}
+
+function changeColors(color) {
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++) {
+		//change each color to match given
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	var rng = Math.floor(Math.random() * colors.length);
+	return colors[rng];
+}
+
+function generateRandomColors(num) {
+	//make an array
+	var arr = []
+	//add num random colors to array
+	for (var i = 0; i < num; i++) {
+		//get random color and push to array
+		arr.push(randomColor());
+	}
+	//return that array
+	return arr;
+}
+
+function randomColor() {
+	//pick a "red" from 0 to 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a "grn" from 0 to 255
+	var g = Math.floor(Math.random() * 256);
+	//pick a "blu" from 0 to 255
+	var b = Math.floor(Math.random() * 256);
+	
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
