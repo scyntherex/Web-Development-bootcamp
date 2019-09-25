5196c8197b04cc5baba9f86c924c3dd9ffca904a
unknown <rone.caballero@gmail.com>
2019-09-25 11:00:54 -0700

    Added basic functionality and contents.

--- todoList/assets/js/todos.js
+++ todoList/assets/js/todos.js
@@ -1,1 +1,17 @@
-alert("CONNECTED");
+// check off specific todis by clicking
+$("li").click(function(){
+	//if li is gray
+	if($(this).css("color") === "rgb(128, 128, 128)"){
+		//turn it black
+		$(this).css({
+			color: "black",
+			textDecoration: "none" 
+		});
+	} else {
+		//turn it gray
+		$(this).css({
+			color: "gray",
+			textDecoration: "line-through" 
+		});
+	}
+});
