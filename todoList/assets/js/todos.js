febcb7b5f3ec41aacd9478cf8c4e5911bd98ce6f
unknown <rone.caballero@gmail.com>
2019-09-26 14:38:04 -0700

    Implemented add to list function. Updated remove function to remove user's added items.

--- todoList/assets/js/todos.js
+++ todoList/assets/js/todos.js
@@ -1,26 +1,22 @@
 // check off specific todis by clicking
-$("li").click(function(){
-	/*//if li is gray
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		//turn it black
-		$(this).css({
-			color: "black",
-			textDecoration: "none" 
-		});
-	} else {
-		//turn it gray
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through" 
-		});
-	}*/
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();	
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	//if the user hits 'enter'
+	if(event.which === 13) {
+		//create new li and add to ul
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
