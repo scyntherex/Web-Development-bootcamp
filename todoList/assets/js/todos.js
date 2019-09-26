ea9de5174219c3d3aedd7c5b15866d718ba7084d
unknown <rone.caballero@gmail.com>
2019-09-26 14:10:32 -0700

    Added remove function in todo list while preventing event bubbling.

--- todoList/assets/js/todos.js
+++ todoList/assets/js/todos.js
@@ -1,6 +1,6 @@
 // check off specific todis by clicking
 $("li").click(function(){
-	//if li is gray
+	/*//if li is gray
 	if($(this).css("color") === "rgb(128, 128, 128)"){
 		//turn it black
 		$(this).css({
@@ -13,5 +13,14 @@ $("li").click(function(){
 			color: "gray",
 			textDecoration: "line-through" 
 		});
-	}
+	}*/
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("span").click(function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();	
+	});
+	event.stopPropagation();
 });
