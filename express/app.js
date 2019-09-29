0b6e0697de7f1a38929ca16e76bd976cc7622315
scyntherex <rone.caballero@gmail.com>
2019-09-29 21:09:26 +0000

    Tested Express framwork.

---
+++ express/app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+// "/" => "Hello!"
+app.get("/", function(req, res) {
+	res.send("Hi there!");
+});
+
+// "/bye" => "Goodbye!"
+app.get("/bye", function(req, res) {
+	res.send("GOODBYE!");
+})
+
+// "/dog" => "MEOW!"
+app.get("/dog", function(req, res) {
+	console.log("Dog was called!");
+	res.send("MEOW!");
+})
+
+//Tell express to listen for requests.
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
+	console.log("Server listening from 3000");
+});
