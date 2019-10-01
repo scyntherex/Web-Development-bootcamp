02c9008701a37779935b6354f54d944877599d5b
scyntherex <rone.caballero@gmail.com>
2019-10-01 03:22:16 +0000

    Updated express library.

--- express/app.js
+++ express/app.js
@@ -9,13 +9,17 @@ app.get("/", function(req, res) {
 // "/bye" => "Goodbye!"
 app.get("/bye", function(req, res) {
 	res.send("GOODBYE!");
-})
+});
 
 // "/dog" => "MEOW!"
 app.get("/dog", function(req, res) {
 	console.log("Dog was called!");
 	res.send("MEOW!");
-})
+});
+
+app.get("*", function(req, res){
+	res.send("YOU ARE A STAR!!!");
+});
 
 //Tell express to listen for requests.
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
