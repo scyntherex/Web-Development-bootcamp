4b0162644c0806b76bc8aaf2f531c6194b7d7c8a
scyntherex <rone.caballero@gmail.com>
2019-10-02 04:49:50 +0000

    Started embedded styling  lecture.

--- moreExpress/EJSDemo/app.js
+++ moreExpress/EJSDemo/app.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-	res.render("index.ejs");
+	res.render("index");
 	//res.send("<h1>Home PAGE</h1>");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
 	var thing = req.params.thing;
-	res.render("love.ejs", {thingVar: thing});
+	res.render("love", {thingVar: thing});
 })
 
 app.get("/posts", function(req, res){
@@ -18,7 +21,7 @@ app.get("/posts", function(req, res){
 		{title: "Avenger", author: "Kevin Feige"},
 	];
 	
-	res.render("posts.ejs", {posts: posts});
+	res.render("posts", {posts: posts});
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
