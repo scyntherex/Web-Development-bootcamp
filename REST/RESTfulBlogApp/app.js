41e886dda231098363badfb5fb28308d770ceaef
scyntherex <rone.caballero@gmail.com>
2019-10-14 22:53:00 +0000

    Created show functionality. Truncated preview text to 100 characters.

--- REST/RESTfulBlogApp/app.js
+++ REST/RESTfulBlogApp/app.js
@@ -52,6 +52,17 @@ app.post("/blogs", function(req, res){
 	});
 });
 
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("show", {blog: foundBlog});
+		}
+	})
+});
+
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
 	console.log("Blog app server running...");
 })
