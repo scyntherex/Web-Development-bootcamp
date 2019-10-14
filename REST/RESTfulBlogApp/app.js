6a350767233a73b6a1d5bc865d3e1c515de74d13
scyntherex <rone.caballero@gmail.com>
2019-10-14 22:11:11 +0000

    Added new post functionality in the blog app.

--- REST/RESTfulBlogApp/app.js
+++ REST/RESTfulBlogApp/app.js
@@ -22,6 +22,8 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req, res){
 	res.redirect("/blogs");
 });
+
+//INDEX ROUTE
 app.get("/blogs", function(req, res){
 	Blog.find({}, function(err, blogs){
 		if(err){
@@ -30,6 +32,24 @@ app.get("/blogs", function(req, res){
 			res.render("index", {blogs: blogs});
 		}
 	})
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+	//create blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err) {
+			res.render("new");
+		} else {
+			//redirect
+			res.redirect("/blogs");
+		}
+	});
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
