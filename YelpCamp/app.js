f372ab39294d0f46f611c66787940c2fd947de53
scyntherex <rone.caballero@gmail.com>
2019-10-26 02:59:47 +0000

    Comment feature functional.

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,6 +3,7 @@ var express 	= require("express"),
 	bodyParser 	= require("body-parser"),
 	mongoose 	= require("mongoose"),
 	Campground  = require("./models/campground"),
+	Comment		= require("./models/comment"),
 	seedDB		= require("./seeds");
 
 seedDB();
@@ -70,6 +71,29 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 			res.render("comments/new", {campground: campground});
 		}
 	})
+})
+
+app.post("/campgrounds/:id/comments", function(req, res){
+	//lookup campground using ID
+	Campground.findById(req.params.id, function(err, campground){
+		if(err)	{
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			//create new comment
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+				} else {
+					//connect new comment to campground
+					campground.comments.push(comment);
+					campground.save();
+					//redirect campground showpage
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			})
+		}	
+	});
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
