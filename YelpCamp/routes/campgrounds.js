6c6a090c22a872fb44666f048cd9ee9d91521c68
scyntherex <rone.caballero@gmail.com>
2019-11-22 01:03:09 +0000

    Prevented users from editing post they did not post.

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -56,13 +56,9 @@ router.get("/:id", function(req, res){
 });
 
 //EDIT CAMPGROUND
-router.get("/:id/edit", function(req, res){
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
 	Campground.findById(req.params.id, function(err, foundCampground){
-		if(err){
-			res.redirect("/campgrounds");
-		} else {
-			res.render("campgrounds/edit", {campground: foundCampground});
-		}
+		res.render("campgrounds/edit", {campground: foundCampground});
 	});
 });
 
@@ -96,6 +92,25 @@ function isLoggedIn(req, res, next){
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+	if(req.isAuthenticated()){
+		Campground.findById(req.params.id, function(err, foundCampground){
+			if(err){
+				res.redirect("back");
+			} else {
+				//does user own the campground?
+				if(foundCampground.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.redirect("back");
+				}
+			}
+		});
+	} else {
+		res.redirect("back");
+	}
 }
 
 module.exports = router;
